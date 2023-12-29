2d741ca6b40fa2b0678622d2d9e9a68132e8f3d4
valditech <valdinoel88@gmail.com>
2023-12-29 15:27:41 -0500

    reactJs

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+export default function () {
+  return (
+    <div className="app">
+      <img src="/download.png"></img>
+      <h1>I learn react</h1>
+      <p>React is amizing</p>
+      <div>
+        <button>My button</button>
+        <button>I love react</button>
+      </div>
+      <hr></hr>
+      <h2>Do you like react?</h2>
+      <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Velit
+        distinctio eaque quibusdam, modi quia error sequi quasi excepturi, odio
+        ratione saepe. Molestias veniam temporibus accusantium libero,
+        consectetur nesciunt vel quia.
+      </p>
+      <ul>
+        <li>Je suis Valdi</li>
+      </ul>
+    </div>
+  );
+}
